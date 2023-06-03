4839c0143b2d9c377d6e1b768817d561ee293f54
taras-fid <taras.fid@gmail.com>
2023-06-03 10:39:48 +0300

    login, redo migrations

---
+++ bot/handlers/users/login.py
@@ -0,0 +1,53 @@
+from aiogram import types
+from loader import dp, bot
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+import re
+from utils.database_connector import cursor, connection_database
+from werkzeug.security import check_password_hash
+from .register import regex_email
+from .login_register_keyboard import kb
+
+
+class LoginStatesGroup(StatesGroup):
+
+    email = State()
+    password = State()
+
+
+@dp.callback_query_handler(text='login_callback')
+async def register_callback_function(call: types.CallbackQuery):
+    await call.answer('Let check is it you! Enter ur login pls <3')
+    await LoginStatesGroup.email.set()
+
+
+@dp.message_handler(state=LoginStatesGroup.email)
+async def username_state_function(message: types.Message, state: FSMContext):
+    if re.fullmatch(regex_email, message.text):
+        sql = f"SELECT `password_hash` FROM `user` WHERE `email` = '{message.text}';"
+        cursor.execute(sql)
+        password = cursor.fetchone()
+        if password:
+            async with state.proxy() as data:
+                data['password'] = password[0]
+                data['false_password_counter'] = 0
+            await message.answer('Now pls enter ur password\nDon`t worry nobody will see it :-)')
+            await LoginStatesGroup.next()
+        else:
+            await message.answer('there is no user with this email, try again')
+    else:
+        await message.answer('email must be like "example@email.com"')
+
+
+@dp.message_handler(state=LoginStatesGroup.password)
+async def password_state_function(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        if data['false_password_counter'] != 3:
+            if check_password_hash(data['password'], message.text):
+                await message.answer('login passed!')
+                await state.reset_state()
+            else:
+                await message.answer('wrong password.')
+                data['false_password_counter'] += 1
+        else:
+            await message.answer('you are blocked... trust this msg pls, GO OUT BASTARD!')
