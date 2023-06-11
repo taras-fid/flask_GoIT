1ea679f3b6ddd38e2acff83c6ab164d4bbb3f985
taras <taras.fid@gmail.com>
2023-06-11 13:25:47 +0300

    comment config. paste urs.

--- bot/utils/database_connector.py
+++ bot/utils/database_connector.py
@@ -1,4 +1,5 @@
 from mysql.connector import connection
 
-connection_database = connection.MySQLConnection(user='root', password='', host='localhost', database='flask_database')
-cursor = connection_database.cursor()
+# connection_database = connection.MySQLConnection(user='root', password='', host='localhost', database='flask_database')
+# cursor = connection_database.cursor()
+# TODO: paste ur database connection config. (it is file: ../config.py).
